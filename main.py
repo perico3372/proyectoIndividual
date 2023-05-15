e7290e18b7600531b4f57a8ba0972a532ca8d54f
perico3372 <117478891+perico3372@users.noreply.github.com>
2023-05-15 16:43:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,10 +28,12 @@ dataframe_movie = pd.read_csv(movie_file)#, usecols=range(11))
 dataframe_production =  pd.read_csv(production_file)#, usecols=range(11))
 dataframe_country =  pd.read_csv(country_file)#, usecols=range(11))
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+#if __name__ == "__main__":
+#    import uvicorn
+#    uvicorn.run(app, host="0.0.0.0", port=10000)
 
+ 
+    
 @app.get("/mes/{month}")
 async def peliculas_month(month: str):
     movies_month = dataframe_movie[dataframe_movie['month'] == month]
